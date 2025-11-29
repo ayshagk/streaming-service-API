b177cc44cf8e2920f0d1c7411f7b9a0cc3a78552
catalinaaxsantana@gmail.com <catalinaaxsantana@gmail.com>
2025-11-29 21:56:43 +0000

    Added user model structs

--- go.mod
+++ go.mod
@@ -4,5 +4,12 @@ go 1.25.1
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
-	golang.org/x/crypto v0.45.0 // indirect
+	golang.org/x/crypto v0.45.0
+	gorm.io/gorm v1.31.1
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.31.0 // indirect
 )
--- models/user.go
+++ models/user.go
@@ -1,1 +1,32 @@
 package models
+
+import "gorm.io/gorm"
+
+type User struct { 
+	gorm.Model
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Age      int    `json:"age"`
+	Password string `json:"password"`
+	Role     string `json:"role"`
+}
+
+type RegisterUser struct {
+	Name     string `json:"name" binding:"required"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Age      int    `json:"age" binding:"required"`
+}
+
+type UserLogin struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type UserResponse struct {
+	ID       uint   `json:"id"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Age      int    `json:"age"`
+	Role     string `json:"role"`
+}
